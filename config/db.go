f9f347095ddb54e995789be9e19468cb94c2420f
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-10 03:27:15 +0700

    connection fixed

--- config/db.go
+++ config/db.go
@@ -17,7 +17,7 @@ func ConnectDB() {
 		fmt.Println("Error loading .env file")
 	}
 
-	dsn := fmt.Sprintln(
+	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
