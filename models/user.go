a43ed7b7295a899cc045cc862713dcead8a2f627
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-10 08:02:25 +0700

    models and utils done

--- models/user.go
+++ models/user.go
@@ -6,5 +6,5 @@ type User struct {
 	gorm.Model
 	Username	string `json:"username" gorm:"unique;not null"`
 	Email		string `json:"email" gorm:"unique;not null"`
-	Password 	string `json:"password" gorm:"not null`
+	Password 	string `json:"password" gorm:"not null"`
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,28 @@
 package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GenerateJWT(userId uint) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userId,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
