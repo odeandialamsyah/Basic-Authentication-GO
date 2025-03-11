b6fc959aa9f66c49834c86b8febb1c1c7b5c9563
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-11 22:34:34 +0700

    authController Register

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"basic_authentication_go/models"
+	"basic_authentication_go/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type AuthController struct {
+	DB *gorm.DB
+}
+
+func (ac *AuthController) Register(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//Hash Password
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	user.Password = hashedPassword
+
+	//save
+	ac.DB.Create(&user)
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
