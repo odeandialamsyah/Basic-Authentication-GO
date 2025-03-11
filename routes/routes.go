fd1e6e2fed41f547b49ae22c869f0e418514939f
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-11 23:57:20 +0700

    routes done

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+    "auth-go/controllers"
+    "github.com/gin-gonic/gin"
+    "gorm.io/gorm"
+)
+
+func AuthRoutes(router *gin.Engine, db *gorm.DB) {
+    authController := controllers.AuthController{DB: db}
+
+    authGroup := router.Group("/api/auth")
+    {
+        authGroup.POST("/register", authController.Register)
+        authGroup.POST("/login", authController.Login)
+    }
+}
