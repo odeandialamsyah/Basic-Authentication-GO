3b7452d174d89c074b226eeaacb6dc3edd3b1321
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-16 23:44:46 +0700

    models user

--- models/user.go
+++ models/user.go
@@ -7,6 +7,8 @@ type User struct {
 	Username	string `json:"username" gorm:"unique;not null"`
 	Email		string `json:"email" gorm:"unique;not null"`
 	Password 	string `json:"password" gorm:"not null"`
+	RoleID   uint   `json:"role_id" gorm:"not null"`
+    Role     Role   `json:"role" gorm:"foreignKey:RoleID"`
 }
 
 func MigrateUsers(db *gorm.DB) {
