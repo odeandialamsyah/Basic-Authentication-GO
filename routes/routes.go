a97b72964f3317fa78e24a55d9419155ac466ec8
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-17 16:53:58 +0700

    route tambahan

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
-    "basic_authentication_go/controllers"
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
+	"basic_authentication_go/controllers"
+	"basic_authentication_go/middleware"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func AuthRoutes(router *gin.Engine, db *gorm.DB) {
@@ -13,5 +16,13 @@ func AuthRoutes(router *gin.Engine, db *gorm.DB) {
     {
         authGroup.POST("/register", authController.Register)
         authGroup.POST("/login", authController.Login)
+    }
+
+    adminGroup := router.Group("/auth/admin")
+    adminGroup.Use(middleware.RoleMiddleware("admin")) 
+    {
+        adminGroup.GET("/dashboard", func (c *gin.Context)  {
+            c.JSON(http.StatusOK, gin.H{"message": "Welcome to the admin dashboard"})
+        })
     }
 }
