c28fb85776cd1bd7e2f6bf1dfa047433ec98e0b8
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-17 16:54:16 +0700

    role middleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -3,6 +3,8 @@ package middleware
 import (
 	"net/http"
 
+	"basic_authentication_go/config"
+	"basic_authentication_go/models"
 	"basic_authentication_go/utils"
 
 	"github.com/dgrijalva/jwt-go"
@@ -27,6 +29,25 @@ func AuthMiddleware() gin.HandlerFunc {
 
 		claims := token.Claims.(jwt.MapClaims)
 		c.Set("user_id", uint(claims["user_id"].(float64)))
+		c.Next()
+	}
+}
+
+func RoleMiddleware(requiredRole string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID := c.MustGet("user_id").(uint)
+
+		var user models.User
+		if err := config.DB.Preload("Role").First(&user, userID).Error; err != nil{
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		if user.Role.Name != requiredRole {
+			c.JSON(http.StatusForbidden, gin. H {"error": "Access Denied"})
+		}
+
 		c.Next()
 	}
 }
