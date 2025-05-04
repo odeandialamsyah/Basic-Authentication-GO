b4be70e2e95a3b63e4534049b2adb688379d70cb
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-04 23:16:21 +0700

    main fixed

--- main.go
+++ main.go
@@ -16,6 +16,21 @@ func main() {
 
 	r := gin.Default()
 
+	// CORS Middleware
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	routes.AuthRoutes(r, config.DB)
 
 	r.Run(":8080")
